90a92ee60f522c7894e570761704116b1d63c16e
dgodwin <dgodwin@singlestoneconsulting.com>
2015-03-24 11:06:35 -0400

    simplified SendHtml method

--- SendGridHelpers/SendGridHelpers.cs
+++ SendGridHelpers/SendGridHelpers.cs
@@ -36,10 +36,10 @@ namespace SendGridHelpers
         /// <param name="subject">Email subject line.</param>
         /// <param name="body">Html formatted body.</param>
         /// <returns>0 to indicate success.</returns>
-        public static Task SendHtml(string to, string subject, string body)
-        {
-            return SendHtml(to, subject, body, ConfigurationManager.AppSettings["SendGrid.DefaultFrom"]);
-        }
+        //public static Task SendHtml(string to, string subject, string body)
+        //{
+        //    return SendHtml(to, subject, body, ConfigurationManager.AppSettings["SendGrid.DefaultFrom"]);
+        //}
 
         /// <summary>
         /// Sends an html formatted email.
@@ -49,15 +49,20 @@ namespace SendGridHelpers
         /// <param name="body">Html formatted body.</param>
         /// <param name="from">The source email address.</param>
         /// <returns>0 to indicate success.</returns>
-        public static Task SendHtml(string to, string subject, string body, string from)
+        public static Task SendHtml(string to, string subject, string body, string from = "")
         {
             // Set up SendGrid message.
             var sgm = new SendGridMessage()
             {
-                From = new MailAddress(from),
                 Subject = subject,
                 Html = body
             };
+
+            if (string.IsNullOrEmpty(from))
+                sgm.From = new MailAddress(ConfigurationManager.AppSettings["SendGrid.DefaultFrom"]);
+            else
+                sgm.From = new MailAddress(from);
+
             sgm.AddTo(to);
 
             // Send it.
